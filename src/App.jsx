590f36e39f7b17a9377213252a52ebb71deedc09
Lukáš Kratochvíl <kratochvil.lukas9@gmail.com>
2021-01-13 21:26:43 +0100

    info for stakeholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 import './App.css';
-import { Button, Modal, OrderExample } from './components';
+import { Button, Modal, OrderExample, SubSectionTitle } from './components';
 
 const App = () => {
     const [isActive, toggleModal] = useState(false);
@@ -13,6 +13,17 @@ const App = () => {
                 <Button variant="primary" onClick={() => toggleModal(!isActive)}>
                     Open modal
                 </Button>
+                <SubSectionTitle>Nedělal jsem hover tooltip na informaci, nechtěl jsem používat externí knihovny - máte vlastní implementaci?</SubSectionTitle>
+                <SubSectionTitle>Selectboxy, checkboxy je třeba napojit do vaší aplikace, poté začnou fungovat</SubSectionTitle>
+                <SubSectionTitle>Úpravy na email kratochvil.lukas9@gmail.com</SubSectionTitle>
+                <SubSectionTitle>
+                    Repository s kódem k nalezení na &nbsp;
+                    {/* eslint-disable-next-line react/jsx-no-target-blank */}
+                    <a href="https://github.com/progresak/mfmodal" target="_blank">https://github.com/progresak/mfmodal</a>
+                    {' '}
+                    ve složce &nbsp;
+                    <strong>src/components</strong>
+                </SubSectionTitle>
                 <Modal
                     isActive={isActive}
                     portalContainerId="modal"
@@ -22,6 +33,7 @@ const App = () => {
                     <OrderExample />
                 </Modal>
             </header>
+
         </div>
     );
 };
