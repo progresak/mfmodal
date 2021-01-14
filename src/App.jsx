cbc99834ad8a895dd5c454a26ebdae5a1a2b69c9
Lukáš Kratochvíl <kratochvil.lukas9@gmail.com>
2021-01-14 22:56:29 +0100

    Implemented react-modal

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css';
 import { Button, Modal, OrderExample, SubSectionTitle } from './components';
 
 const App = () => {
-    const [isActive, toggleModal] = useState(true);
+    const [isActive, toggleModal] = useState(false);
 
     return (
         <div className="App">
